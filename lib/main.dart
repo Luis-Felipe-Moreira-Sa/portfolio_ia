957f3b089b87540c9845dccfd4c20fb8d182edaa
Luis Felipe Moreira de Sá <104095195+Luis-Felipe-Moreira-Sa@users.noreply.github.com>
2023-05-09 19:12:19 -0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -12,47 +12,46 @@ void main() async {
   // var response =
   //     await http.get(Uri.parse('assets/ticker.json'));
   // var jsonString = response.body;
-  WidgetsFlutterBinding
-      .ensureInitialized(); // Certifique-se de inicializar o Flutter
-  final jsonString = await rootBundle.loadString('assets/ticker.json');
+//  WidgetsFlutterBinding
+//      .ensureInitialized(); // Certifique-se de inicializar o Flutter
+//  final jsonString = await rootBundle.loadString('assets/ticker.json');
 
   // Passo 2: Converter o conteúdo JSON em uma lista de mapas
-  List<Map<String, dynamic>> stockData =
-      List<Map<String, dynamic>>.from(jsonDecode(jsonString));
+//  List<Map<String, dynamic>> stockData =
+//      List<Map<String, dynamic>>.from(jsonDecode(jsonString));
 
   // Passo 3: Utilizar o dicionário stockData conforme necessário
-  for (var stock in stockData) {
-    print(stock['name']);
-    print(stock['ticker']);
-    print(stock['price']);
-    print(stock['price_future']);
-    print(stock['sector']);
-    print(stock['percentage']);
-    print(stock['backgroundColor']);
-    print('------------------');
-  }
-  // runApp(MaterialApp(
-  //   title: "InvestmentPortfolio",
-  //   theme: ThemeData.dark().copyWith(
-  //     // Cor dos ícones
-  //     iconTheme: IconThemeData(
-  //       color: Colors.white, // Cor branca para os ícones
-  //     ),
+//  for (var stock in stockData) {
+//    print(stock['name']);
+//    print(stock['ticker']);
+//    print(stock['price']);
+//    print(stock['price_future']);
+//    print(stock['sector']);
+//    print(stock['percentage']);
+//    print(stock['backgroundColor']);
+//    print('------------------');
+//  }
+   runApp(MaterialApp(
+     title: "InvestmentPortfolio",
+     theme: ThemeData.dark().copyWith(
+       // Cor dos ícones
+       iconTheme: IconThemeData(
+         color: Colors.white, // Cor branca para os ícones
+       ),
+     // Cor dos textos
+     textTheme: TextTheme(
+       bodyLarge: TextStyle(
+         color: Colors.white, // Cor branca para textos principais
+       ),
+       bodyMedium: TextStyle(
+         color: Colors.grey, // Cor cinza claro para textos secundários
+       ),
+     ),
 
-  //     // Cor dos textos
-  //     textTheme: TextTheme(
-  //       bodyLarge: TextStyle(
-  //         color: Colors.white, // Cor branca para textos principais
-  //       ),
-  //       bodyMedium: TextStyle(
-  //         color: Colors.grey, // Cor cinza claro para textos secundários
-  //       ),
-  //     ),
-
-  //     // Cor de fundo do Scaffold
-  //     scaffoldBackgroundColor: Colors.black, // Cor preta para o fundo
-  //   ),
-  //   home: InvestmentPortfolio(),
-  //   debugShowCheckedModeBanner: false,
-  // ));
+     // Cor de fundo do Scaffold
+     scaffoldBackgroundColor: Colors.black, // Cor preta para o fundo
+   ),
+   home: InvestmentPortfolio(),
+   debugShowCheckedModeBanner: false,
+));
 }
